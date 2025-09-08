878cf491e712ca457fe5ea5ccc6c780031ddc28c
Piotr Wierzgała <piotr.wierzgala@gmail.com>
2025-09-08 13:04:48 +0200

    updated cPanel statup file

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'syjon.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
